c26d62da416793a6fd7fc7aad0ebe49e9037b0e6
Krish Shah <shahkrish2016@gmail.com>
2022-07-14 10:16:07 -0400

    built cost vector

--- test_rank.py
+++ test_rank.py
@@ -1,50 +1,43 @@
 
+from text import Node
+
 class TextRank:
 
-    def __init__(self, words: set, d=0.85):
+    def __init__(self, words: list[Node], d=0.85):
         self.words = words
         self.scores = {}
 
-    def tabulate(self):
-        pass
-    
-# use proper implementation later (numpy?)
-def getCol(arr):
-    cols = []
-    for i in range(len(arr[0])):
-        cols.append([])
-        for j in range(len(arr)):
-            cols[-1].append(arr[j][i])
-    return cols
+        # for word in self.words:
+        #     self.cost[word] = 1/self.N
+        
+        self.d = d
+        self.cost = dict()
 
-# q = [[9, 24], [7, 18], [25, 63]]
-# print(getCol(q))
+    def matrix_multiplication(self, a, b):            
+        # use proper implementation later (numpy?)
+        def getCol(arr):
+            cols = []
+            for i in range(len(arr[0])):
+                cols.append([])
+                for j in range(len(arr)):
+                    cols[-1].append(arr[j][i])
+            return cols
 
-def matrix_multiplication(a, b):
-    # len(col of first) = len(row of second) 
-    assert(len(a[0]) == len(b))
+        # len(col of first) = len(row of second) 
+        assert(len(a[0]) == len(b))
 
-    results = []    
-    for row in a:
-        results.append([])
-        for col in getCol(b):
-            current_sum = 0
-            for i in range(len(row)):
-                current_sum += row[i]*col[i]
-            results[-1].append(current_sum)
+        results = []    
+        for row in a:
+            results.append([])
+            for col in getCol(b):
+                current_sum = 0
+                for i in range(len(row)):
+                    current_sum += row[i]*col[i]
+                results[-1].append(current_sum)
 
-    return results
+        return results
 
-x = [
-    [4, -1],
-    [0, 2],
-    [-8, 3]
-]
-
-y = [
-    [6, -2, 1],
-    [5, 0, 7]
-]
-
-a = matrix_multiplication(x, y)
-print(a)
+    # def tabulate(self):
+        
+    #     for i in range(10):
+            
--- text.py
+++ text.py
@@ -2,9 +2,30 @@ from nltk import word_tokenize, sent_tokenize
 from nltk.corpus import stopwords
 import string
 
-from test_rank import TextRank
+from ordered_set import OrderedSet
+
+# from test_rank import TextRank
 
 class Text:
+
+    class Node:    
+        # initialize all words first, then add reference wherever necessary
+        def __init__(self, word):
+            self.word = word
+            self.incoming = set()
+            self.outgoing = set()
+
+        def __repr__(self):
+            info = 'incoming:{'
+            for current_node in self.incoming:
+                info += current_node.word + ', '
+            info = info[:-2]
+            info += '} outgoing:{' 
+            for current_node in self.outgoing:
+                info += current_node.word + ', ' 
+            info = info[:-2]
+            info += '}\n'
+            return info
 
     unwanted_snippets = ["'s"]
 
@@ -13,7 +34,9 @@ class Text:
         
         # method calls
         self.extract_words()
-        self.score_sentences()
+        self.build_graph()
+        self.build_vector()
+        # self.score_sentences()
 
     # lowercase, remove stop words, extract unique words
     def extract_words(self):
@@ -28,12 +51,62 @@ class Text:
                     continue
                 self.words[-1].append(revised)
 
-        self.unique_words = set()
+        self.unique_words = OrderedSet()
         for sentence in self.words:
             for word in sentence:
                 self.unique_words.add(word)
+        self.unique_words = list(self.unique_words)
 
-        print(a.unique_words)
+    def build_graph(self, window_size=4):
+        self.graph = dict()
+
+        # initialize all words
+        for word in self.unique_words:
+            self.graph[word] = Text.Node(word)
+
+        # set ingoing and outgoing words
+        self.windows = []
+        # print(self.unique_words)
+        for i in range(len(self.unique_words)-window_size+1):
+            self.windows.append(self.unique_words[i:i+window_size])
+        
+        # print(self.windows)
+
+        for window in self.windows:
+            for i in range(len(window)):
+                for j in range(len(window)):
+                    if i == j:
+                        continue
+                    # edge from first_word to second_word
+                    first_word, second_word = window[i], window[j]
+                    self.graph[first_word].outgoing.add(self.graph[second_word])
+                    self.graph[second_word].incoming.add(self.graph[first_word])
+        
+        for word, current_node in self.graph.items():
+            # print(word, current_node)
+            current_node.outgoing = list(current_node.outgoing)
+            current_node.incoming = list(current_node.incoming)
+            # print(type(current_node.outgoing))
+        
+        # print(self.graph)
+
+    def build_vector(self):
+        self.costs = dict()
+        # keys are all strings 
+        for first_word in self.graph:
+            self.costs[first_word] = dict() 
+            for second_word in self.graph:
+                if first_word == second_word:
+                    continue
+                self.costs[first_word][second_word] = 0
+
+        # test numbers later with a specific example, for now just assuming that this works 
+        for first_word in self.costs:
+            for existing_word in self.graph[first_word].outgoing:
+                print(type(existing_word))
+                self.costs[first_word][existing_word] = 1/len(self.graph[first_word].outgoing)
+
+        print(self.costs)
 
     def score_sentences(self):
         ranker = TextRank(self.unique_words)
@@ -57,5 +130,7 @@ class Text:
         return summary
 
 a = Text("Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.")
+
+# print(a.vector)
 
 # summary = a.summarize()
